f099c1b8b538bec7da54878763da5bf365570b02
Drakoxw <developer17@sinergiaonline.com>
2025-10-02 19:07:57 -0500

    feat: the example component is deleted

--- src/app/modules/home/index.ts
+++
@@ -1,8 +0,0 @@
-import { ExampleMapComponent } from './example-map/example-map.component'
-
-export const HOME_COMPONENTS = [
-  ExampleMapComponent
-]
-
-export * from './example-map/example-map.component'
-
