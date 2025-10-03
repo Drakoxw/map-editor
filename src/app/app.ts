f7757151aed9fb41018153a87b9de0368ec0131d
Drakoxw <developer17@sinergiaonline.com>
2025-10-02 19:06:47 -0500

    feat: initial code is deleted and the app theme is adjusted

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterOutlet],
 })
-export class App {
-  protected readonly title = signal('MapLibre');
+export class App implements OnInit {
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    this.renderer.removeClass(this.document.documentElement, 'dark');
+    this.renderer.addClass(this.document.documentElement, 'light');
+  }
 }
