ac0129bb7cefed1696733f40684333766db08a5d
Drakoxw <developer17@sinergiaonline.com>
2025-10-02 19:07:20 -0500

    feat: routes are added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PATH } from '@constants/index';
+import { LayoutComponent } from '@shared/components';
+
+import MapEditorComponent from '@poi-editor-module/map-editor/map-editor.component';
+import HomeIndexComponent from '@home-module/home-index/home-index.component';
+import AboutIndexComponent from '@about-module/about-index/about-index.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: PATH.HOME, component: HomeIndexComponent },
+      { path: PATH.EDITOR, component: MapEditorComponent },
+      { path: PATH.ABOUT, component: AboutIndexComponent },
+      { path: '**', redirectTo: PATH.HOME, pathMatch: 'full' },
+      { path: '', redirectTo: PATH.HOME, pathMatch: 'full' },
+    ],
+  },
+];
